04a678ad93422e3823683d873eae84db8d93e7ba
kimdoyeon <lemon8818@naver.com>
2024-09-19 16:57:48 +0900

    fix: css수정

--- frontend/src/js/ProductDetails.js
+++ frontend/src/js/ProductDetails.js
@@ -1,6 +1,5 @@
 import React, { useEffect,useState } from 'react';
 import axios from 'axios';
-import '../css/GetItem.css';
 import Header from './Header';
 import { useParams } from 'react-router-dom';
 
